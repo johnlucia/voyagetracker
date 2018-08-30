d1d6a2d6d88492183df343eaaee539522a9fc5f4
johnlucia <johnmlucia@gmail.com>
2018-08-29 20:51:19 -0700

    hacking along

--- app/lib/email_processor.rb
+++ app/lib/email_processor.rb
@@ -5,16 +5,40 @@ class EmailProcessor
 
   def process
     boat = Boat.find_by_tracking_email(@email.from[:email])
+
     puts "/n/n/n/n"
     puts @email.from[:email]
     puts "/n/n/n/n"
     puts boat.inspect
     puts "/n/n/n/n #{@email.body} /n/n/n/n"
 
+    return unless coords = lat_lon_from_text(@email.body)
+
     boat.positions.create(
-      latitude: 1.111,
-      longitude: -123.16056
+      latitude: coords[:lat],
+      longitude: coords[:lon]
     )
+  end
+
+  private
+
+  def lat_lon_from_text(text)
+    ll_regex=/(latitude|longitude|long|lat|lon|longitude|lng)(\D*-?)(0|[1-9]\d*\.\d+)?/i
+    matches = text.scan(ll_regex)
+
+    return nil unless matches
+
+    lat_entry = matches.find { |entry| entry[0] =~ /^lat/i }
+    lon_entry = matches.find { |entry| entry[0] =~ /^(lon|lng)/i }
+
+    return nil unless lat_entry && lon_entry
+
+    latitude_value = "#{'-' if lat_entry[1].include?("-")}#{lat_entry.last}".to_f
+    longitude_value = "#{'-' if lon_entry[1].include?("-")}#{lon_entry.last}".to_f
+
+    return nil unless latitude_value && longitude_value
+
+    { lat: latitude_value, lon: longitude_value }
   end
 
 
