59a13404b07dce586bb2724947886d3cb4e035b2
johnlucia <johnmlucia@gmail.com>
2018-09-08 06:53:47 -0700

    admin stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :admin_user_check
+
+  def admin_user_check
+    @admin_user = current_user&.admin
+  end
 
   protected
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,20 @@ class User < ApplicationRecord
 
   has_many :boats
 
+  validates :email, uniqueness: true
+  validate  :new_user_has_valid_account_key, :on => :create
+
   after_create :associate_boat
 
   def associate_boat
     users_boat = Boat.find_by_user_key(account_key)
     users_boat.update_attributes(user_id: id)
+  end
+
+  def new_user_has_valid_account_key
+    users_boat = Boat.find_by_user_key(account_key)
+    unless !account_key.blank? && users_boat
+      errors.add(:account_key, "is not valid")
+    end
   end
 end
