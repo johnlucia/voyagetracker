9c3fe6057a625c3c6753890cbe692fc56dd3cd3c
johnlucia <johnmlucia@gmail.com>
2019-01-02 07:49:44 -0700

    add meta to json

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -3,9 +3,17 @@ class PositionsController < ApplicationController
   def index
     @boat = Boat.find params[:boat_id]
     @positions = @boat.positions.order(created_at: :desc)
+    @summary = {
+      last_update: @boat.last_update,
+      speed_in_knots: @boat.speed_in_knots,
+      heading: @boat.heading,
+      status: @boat.status,
+      latitude: @boat.latitude,
+      longitude: @boat.longitude
+    }
     respond_to do |format|
       format.html { render :index }
-      format.json { render json: @positions, root: 'route', adapter: :json }
+      format.json { render json: @positions, root: 'route', adapter: :json, meta: @summary }
     end
   end
 end
